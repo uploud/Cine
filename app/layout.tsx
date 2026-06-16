5cd91495190a547a69ce142684e9b485ac91b10a
CineTube <user@cinetube.ia>
2026-06-16 01:23:36 -0300

    feat: initialize root layout with custom fonts and Facebook Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '952543821027667');
+            fbq('init', '897100243406956');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -49,7 +49,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=952543821027667&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=897100243406956&ev=PageView&noscript=1"
           />
         </noscript>
         {children}
